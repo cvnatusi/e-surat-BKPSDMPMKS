d1f089928baff6cf0abce83ea514e3ca806a8135
fatkhan05 <watsittoya469@gmail.com>
2023-11-09 15:27:16 +0700

    add realtion in surat tugas

--- app/Models/SuratTugas.php
+++ app/Models/SuratTugas.php
@@ -8,28 +8,38 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 use Haruncpi\LaravelUserActivity\Traits\Loggable;
 class SuratTugas extends Model
 {
-  use SoftDeletes,Loggable;
+	use SoftDeletes,Loggable;
 
-  protected $table = 'tr_surat_perjalanan_dinas';
-  protected $primaryKey = 'id_surat_perjalanan_dinas';
-  public $timestamps = true;
-  protected $dates = ['deleted_at'];
+	protected $table = 'tr_surat_perjalanan_dinas';
+	protected $primaryKey = 'id_surat_perjalanan_dinas';
+	public $timestamps = true;
+	protected $dates = ['deleted_at'];
 
-  public function suratkeluar()
-  {
-      return $this->belongsTo('App\Models\SuratKeluar', 'surat_keluar_id');
-  }
-  public function pegawai()
-  {
-      return $this->belongsTo('App\Models\MasterASN', 'asn_id');
-  }
-  public function tujuan()
-  {
-      return $this->hasMany('App\Models\TujuanSuratTugas', 'surat_tugas_id', 'id_surat_perjalanan_dinas');
-  }
-  public function penandatangan()
-  {
-      return $this->belongsTo('App\Models\MasterASN', 'yang_bertanda_tangan_asn_id');
-  }
+	// public function getAsnIdAttribute($value){
+	// 	return (int)$value;
+	// }
+// 	protected $casts = [
+// 		'asn_id' => 'integer',
+//   ];
 
+	public function suratkeluar()
+	{
+		return $this->belongsTo('App\Models\SuratKeluar', 'surat_keluar_id');
+	}
+	public function pegawai()
+	{
+		return $this->belongsTo('App\Models\MasterASN', 'asn_id', 'id_mst_asn');
+	}
+	// public function pegawai()
+	// {
+	// 	return $this->belongsTo('App\Models\MasterASN', 'asn_id');
+	// }
+	public function tujuan()
+	{
+		return $this->hasMany('App\Models\TujuanSuratTugas', 'surat_tugas_id', 'id_surat_perjalanan_dinas');
+	}
+	public function penandatangan()
+	{
+		return $this->belongsTo('App\Models\MasterASN', 'yang_bertanda_tangan_asn_id');
+	}
 }
